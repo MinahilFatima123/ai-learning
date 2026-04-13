9cac64997875e3d12202c27f40e8aedd35f976e4
MinahilFatima123 <fatimaminahil528@gmail.com>
2026-04-13 23:50:15 +0500

    chatbot project done

---
+++ phase2/phase2.py
@@ -0,0 +1,26 @@
+from groq import Groq
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+client = Groq(api_key=os.getenv("GROQ_API_KEY"))
+
+question = "Write me a one sentence story"
+
+for temp in [0.0, 0.7, 1.5]:
+    response = client.chat.completions.create(
+        model="llama-3.3-70b-versatile",
+        messages=[{"role": "user", "content": question}],
+        temperature=temp
+    )
+    print(f"Temp {temp}: {response.choices[0].message.content}")
+    print("---")
+response = client.chat.completions.create(
+    model="llama-3.3-70b-versatile",
+    messages=[
+        {"role": "user", "content": "Who won the Pakistan vs India cricket match on 5th April 2026?"}
+    ],
+    temperature=0.7
+)
+print(response.choices[0].message.content)
--- phase2/project.py
+++ phase2/project.py
@@ -6,14 +6,34 @@ load_dotenv()
 
 client = Groq(api_key=os.getenv("GROQ_API_KEY"))
 
-question = "Write me a one sentence story"
+messages = [
+    {"role": "system", "content": "You are a helpful assistant"}
+]
 
-for temp in [0.0, 0.7, 1.5]:
+while True:
+    user_input = input("You: ")
+    
+    if user_input == "quit":
+        print("Goodbye!")
+        break       
+    
+    messages.append({  
+        "role": "user", 
+        "content": user_input
+    })
+    
     response = client.chat.completions.create(
         model="llama-3.3-70b-versatile",
-        messages=[{"role": "user", "content": question}],
-        temperature=temp
+        messages=messages,
+        temperature=0.7
     )
-    print(f"Temp {temp}: {response.choices[0].message.content}")
-    print("---")
-
+    
+    reply = response.choices[0].message.content  
+    
+    print(f"Assistant: {reply}")
+    
+    messages.append({
+        "role": "assistant",
+        "content": reply
+    })
+  
